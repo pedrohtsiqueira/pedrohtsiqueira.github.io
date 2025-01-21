f9aa7b7985d27bf9d81b9985c497cced2a070423
unknown <pedrohtsiqueira@gmail.com>
2025-01-20 21:04:14 -0300

    last changes until start to create my own projects - 20/01/2025

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,14 @@ import Projeto from './Projeto';
 export default function Portfolio () {
     return (
         <div className="Portfolio">
-            <Projeto/>
+            <h2>Projetos Realizados</h2>
+            <div className='container'>
+                <Projeto/>
+                <Projeto/>
+                <Projeto/>
+                <Projeto/>
+                <Projeto/>
+            </div>
         </div>
     )
 }
