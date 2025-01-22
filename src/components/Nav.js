5ee7e49d71a3de61c2595c83125651d075620c11
unknown <pedrohtsiqueira@gmail.com>
2025-01-22 07:29:06 -0300

    nav bar for mobile - 22/01/2025

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
+import { IoMdMenu } from "react-icons/io";
+
 
 
 export default function Nav () {
     return (
-        <nav>
-            <ul>
-                <Link to='/'>Home</Link>
-                <Link to='About'>Sobre mim</Link>
-                <Link to='Portfolio'>Projetos</Link>
-            </ul>
-        </nav>
+        <>
+            <nav className="desktop">
+                <ul>
+                    <Link to='/'>Home</Link>
+                    <Link to='About'>Sobre mim</Link>
+                    <Link to='Portfolio'>Projetos</Link>
+                </ul>
+            
+            </nav>
+            <nav className="mobile">
+                <IoMdMenu/>
+            </nav>
+        </>
+
     )
 }
